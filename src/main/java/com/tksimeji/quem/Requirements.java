5cc02880d009ef5c681385e81b1b059213c6e780
tksimeji <tksimeji@outlook.com>
2025-01-08 22:14:38 +0900

    fix: change to the appropriate annotation

--- src/main/java/com/tksimeji/quem/Requirements.java
+++ src/main/java/com/tksimeji/quem/Requirements.java
@@ -2,19 +2,23 @@ package com.tksimeji.quem;
 
 import com.google.gson.JsonObject;
 import org.bukkit.configuration.ConfigurationSection;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashSet;
 import java.util.Optional;
 
 public final class Requirements extends HashSet<IQuestType.Requirement> {
-    public Requirements(@NotNull JsonObject json) {
+    public Requirements(@Nullable JsonObject json) {
         Optional.ofNullable(json).orElse(new JsonObject()).entrySet().stream()
                 .filter(entry -> entry.getValue().isJsonPrimitive() && entry.getValue().getAsJsonPrimitive().isNumber())
                 .forEach(entry -> add(new IQuestType.Requirement(entry.getKey(), entry.getValue().getAsInt())));
     }
 
-    public Requirements(@NotNull ConfigurationSection yaml) {
+    public Requirements(@Nullable ConfigurationSection yaml) {
+        if (yaml == null) {
+            return;
+        }
+
         yaml.getValues(false).entrySet().stream()
                 .filter(entry -> entry.getValue() instanceof Integer)
                 .forEach(entry -> add(new IQuestType.Requirement(entry.getKey(), (Integer) entry.getValue())));
